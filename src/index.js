7c50605dfca96f43a3737d9433b1d9997642199f
kioryu <1218moonlight@gmail.com>
2019-09-17 18:06:49 +0900

    [ ADD ] URL Cookie

--- src/index.js
+++ src/index.js
@@ -9,12 +9,20 @@ let accountsInfo = document.querySelector('#accountsInfo');
 
 let web3 = false;
 
+let urlCookieKey = "urlCookie";
+
+let init = getUrlCookie(urlCookieKey);
+if (init !== null) {
+    gethUrl.value = init
+}
+
 gethConnectBtn.onclick = () => {
     web3 = new Web3(new Web3.providers.HttpProvider(gethUrl.value)); // http://192.168.0.204:8545
     try {
         let con = web3.isConnected();
         gethConnectInfo.innerText = "connect (" + con + ")";
         if (con) {
+            setUrlCookie(urlCookieKey, gethUrl.value, 1);
             accountsDiv.style.display = "inline"
         } else {
             accountsDiv.style.display = "none"
@@ -37,7 +45,22 @@ accountsBtn.onclick = () => {
             accountsObj.balance = web3.eth.getBalance(accounts[i]).toString(10);
             accountsObjArray.push(accountsObj)
         }
-        console.log(accountsObjArray);
         accountsInfo.innerText = JSON.stringify(accountsObjArray)
     }
 };
+
+function setUrlCookie(name, value, day) {
+    let date = new Date();
+    date.setTime(date.getTime() + day * 60 * 60 * 24 * 1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+
+function getUrlCookie(name) {
+    let value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value ? value[2] : null;
+}
+
+function deleteUrlCookie(name) {
+    let date = new Date();
+    document.cookie = name + "= " + "; expires=" + date.toUTCString() + "; path=/";
+}
